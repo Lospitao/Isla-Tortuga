f37e3be137bc67d13873d3a3c8a116c88e8f5a92
Laura LOspitao <lauralospitao@gmail.com>
2020-05-18 19:03:52 +0200

    #49 Unlike buton handler Added and working

--- like.php
+++ like.php
@@ -4,6 +4,18 @@
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
+<style type="text/css">
+    * {
+        font-family: Arial, Helvetica, Sans-serif;
+    }
+    body {
+        background-color: #fff;
+    }
+    form {
+        position: absolute;
+        top:0;
+    }
+</style>
     <?php
     require_once 'config/config.php';
     include("includes/classes/User.php");
