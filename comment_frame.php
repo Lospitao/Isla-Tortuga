ec89d507ba3acd947e0d8cb3c05f80a6e602a3c3
Laura LOspitao <lauralospitao@gmail.com>
2020-05-18 11:34:52 +0200

    #42 Comment a post Comments on a post are showed in descending order (the older is the first one) when clicking on post space, profile_pic and first and last name of the person commenting are showed and both linked to profile page which is opened separately now

--- comment_frame.php
+++ comment_frame.php
@@ -128,11 +128,19 @@
                 }
             }
             $user_obj = new User($conn, $posted_by);
+
+            ?>
+            <div class="comment_section">
+                <a href="<?php echo $posted_by?>" target="_parent"><img src="<?php echo $user_obj->getProfilePic(); ?>" title="<?php echo $posted_by; ?>" style="float:left;" height="30"></a>
+                <a href="<?php echo $posted_by?>" target="_parent"><b><?php echo $user_obj->getFirstAndLastName();?></b></a>
+                &nbsp;&nbsp;&nbsp;&nbsp;<?php echo $time_message . "<br>" . $comment_body; ?>
+                <hr>
+            </div>
+
+            <?php
         }
     }
     ?>
-    <div class="comment_section">
-        <a href="<?php echo $posted_by?>" target="_parent"><?php echo $posted_by?></a>
-    </div>
+
 </body>
 </html>
