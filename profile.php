73dc6db41d71081f22cef0c3e113dc01e78daa16
Laura LOspitao <lauralospitao@gmail.com>
2020-05-20 17:42:03 +0200

    #52 Styling the profile bar

--- profile.php
+++ profile.php
@@ -10,16 +10,23 @@ if(isset($_GET['profile_username'])) {
     $num_friends = (substr_count($user_array['friend_array'], ",")) -1;
 }
 ?>
+    <style type="text/css">
+        .wrapper {
+            margin-left: 0;
+            padding-left: 0;
+        }
+    </style>
     <div class="profile_left">
         <img src="<?php echo $user_array['profile_pic']?>">
         <div class="profile_info">
-        <p><?php echo "Posts: " . $user_array['num_posts']; ?></p>
-        <p><?php echo "Likes: " . $user_array['num_likes']; ?></p>
-        <p><?php echo "Friends: " . $num_friends; ?></p>
+            <p><?php echo "Posts: " . $user_array['num_posts']; ?></p>
+            <p><?php echo "Likes: " . $user_array['num_likes']; ?></p>
+            <p><?php echo "Friends: " . $num_friends; ?></p>
         </div>
+    </div>
 
     <div class="main_column column">
-    <?php echo $username?>
+    <?php echo $username; ?>
 
 
 
