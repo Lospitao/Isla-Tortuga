09dca6030dd8e1e7c7e483f9c588ea29f9ff9778
Laura LOspitao <lauralospitao@gmail.com>
2020-05-21 19:18:21 +0200

    #59 Create an Add Friend Functionality

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -54,16 +54,16 @@ class User {
 	        return false;
 	    }
     }
-    public function didReceiveRequest ($user_to) {
-        $user_from = $this->user['username'];
-        $check_request_query = mysqli_query($this->conn, "SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='user_from'");
+    public function didReceiveRequest ($user_from) {
+        $user_to = $this->user['username'];
+        $check_request_query = mysqli_query($this->conn, "SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='$user_from'");
         if (mysqli_num_rows($check_request_query) > 0) {
             return true;
         } else return false;
     }
-	public function didSendRequest ($user_from) {
-	    $user_to = $this->user['username'];
-        $check_request_query = mysqli_query($this->conn, "SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='user_from'");
+	public function didSendRequest ($user_to) {
+	    $user_from = $this->user['username'];
+        $check_request_query = mysqli_query($this->conn, "SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='$user_from'");
         if(mysqli_num_rows($check_request_query) > 0) {
            return true;
         }
@@ -80,6 +80,10 @@ class User {
 
         $new_friend_array = str_replace($this->user['useraname'] . ",", "", $friend_array_username);
         $remove_friend = mysqli_query($this->conn, "UPDATE users SET friend_array='$new_friend_array' WHERE username='$user_to_remove'");
+    }
+    public function sendRequest($user_to) {
+        $user_from = $this->user['username'];
+        $query = mysqli_query($this->conn, "INSERT INTO friend_requests VALUES(NULL, '$user_to', '$user_from')");
     }
 
 }
--- profile.php
+++ profile.php
@@ -11,6 +11,17 @@ if(isset($_GET['profile_username'])) {
 
     $num_friends = (substr_count($user_array['friend_array'], ",")) -1;
 }
+if(isset($_POST['remove_friend'])) {
+    $user = new User($conn, $userLoggedIn);
+    $user->removeFriend($username);
+}
+if(isset($_POST['add_friend'])) {
+    $user = new User($conn, $userLoggedIn);
+    $user->sendRequest($username);
+}
+if(isset($_POST['respond_request'])) {
+    header("Location: requests.php");
+}
 ?>
     <style type="text/css">
         .wrapper {
@@ -25,7 +36,7 @@ if(isset($_GET['profile_username'])) {
             <p><?php echo "Likes: " . $user_array['num_likes']; ?></p>
             <p><?php echo "Friends: " . $num_friends; ?></p>
         </div>
-    <form action="<?php echo $username; ?>">
+    <form action="<?php echo $username; ?>" method="POST">
         <?php
         $profile_user_obj = new User($conn, $username);
         if($profile_user_obj->isClosed()) {
