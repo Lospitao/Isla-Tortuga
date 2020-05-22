40c90e8055c5b4b889f069eac7beeb12dea8ebe2
Laura LOspitao <lauralospitao@gmail.com>
2020-05-22 19:39:46 +0200

    #63 Showing the Popup Post form

--- profile.php
+++ profile.php
@@ -22,22 +22,22 @@ if(isset($_POST['respond_request'])) {
     header("Location: request.php");
 }
 ?>
-    <style type="text/css">
-        .wrapper {
-            margin-left: 0;
-            padding-left: 0;
-        }
-    </style>
-    <div class="profile_left">
-        <img src="<?php echo $user_array['profile_pic']?>">
-        <div class="profile_info">
-            <p><?php echo "Posts: " . $user_array['num_posts']; ?></p>
-            <p><?php echo "Likes: " . $user_array['num_likes']; ?></p>
-            <p><?php echo "Friends: " . $num_friends; ?></p>
-        </div>
+<style type="text/css">
+    .wrapper {
+        margin-left: 0;
+        padding-left: 0;
+    }
+</style>
+<div class="profile_left">
+    <img src="<?php echo $user_array['profile_pic']?>">
+    <div class="profile_info">
+        <p><?php echo "Posts: " . $user_array['num_posts']; ?></p>
+        <p><?php echo "Likes: " . $user_array['num_likes']; ?></p>
+        <p><?php echo "Friends: " . $num_friends; ?></p>
+    </div>
     <form action="<?php echo $username; ?>" method="POST">
         <?php
-        $profile_user_obj = new User($conn, $username);
+                $profile_user_obj = new User($conn, $username);
         if($profile_user_obj->isClosed()) {
             header("Location:user_closed.php");
         }
@@ -56,21 +56,53 @@ if(isset($_POST['respond_request'])) {
             else
                 echo '<input type="submit" name="add_friend" class="success" value="Add Friend"><br>';
         }
-        ?>
+                ?>
 
     </form>
-    </div>
+    <input type="submit" class="deep_blue" data-toggle="modal" data-target="#post_form" value="Post Something">
+</div>
 
-    <div class="main_column column">
+<div class="main_column column">
     <?php echo $username; ?>
 
 
 
+</div>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="post_form" tabindex="-1" role="dialog" aria-labelledby="postModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Post Something</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+                <p>This will appear on the user's profile page and also their newsfeed for your friends to see!</p>
+                <form class="profile_post" action="" method="POST">
+                    <div>
+                        <textarea class="form-control" name="post_body"></textarea>
+                        <input type="hidden" name="user_from" value="<?php $userLoggedIn; ?>">
+                        <input type="hidden" name="user_to" value="<?php $username; ?>">
+                    </div>
+
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Post</button>
+            </div>
+        </div>
     </div>
+</div>
 
 
-
-
-    </div>
+</div>
 </body>
 </html>
