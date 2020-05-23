73068476d68ac982cf21877fb4fbc329a11c3b43
Laura LOspitao <lauralospitao@gmail.com>
2020-05-23 17:55:53 +0200

    #64 Create ajax call for Post button in profile In file isla-tortuga.js (assets/js)

--- includes/header.php
+++ includes/header.php
@@ -17,14 +17,17 @@ else {
 <head>
 	<title>Welcome to Tortuga Island</title>
 	<!---Javascript--->
+    <script src="assents/js/isla-tortuga.js">
+    </script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js">
 	</script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
 	</script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-	</script>
-	<!--CSS-->
-	<script src="https://kit.fontawesome.com/5a4a71c487.js" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+
+    <!--CSS-->
+    <script src="https://kit.fontawesome.com/5a4a71c487.js" crossorigin="anonymous"></script>
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
