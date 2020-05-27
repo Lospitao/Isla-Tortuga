71d27ca233035ee9883113dbecd759237f31e5fb
Laura LOspitao <lauralospitao@gmail.com>
2020-05-27 12:04:37 +0200

    #75 Retrieving the Most Recently Interacted with User

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -1,13 +1,35 @@
 <?php
-class Message {
+class Message
+{
     private $user_obj;
     private $conn;
 
-    public function __construct($conn, $user) {
+    public function __construct($conn, $user)
+    {
         $this->conn = $conn;
         $this->user_obj = new User ($conn, $user);
 
     }
 
-}
+    public function getMostRecentUser()
+    {
+        $userLoggedIn = $this->user_obj->getUsername();
 
+        $query = mysqli_query($this->conn, "SELECT user_to, user_from FROM messages WHERE user_to='$userLoggedIn' 
+        OR user_from= '$userLoggedIn' ORDER BY id DESC LIMIT 1");
+
+        if(mysqli_num_rows($query) == 0)
+            return false;
+
+        $row = mysqli_fetch_array($query);
+        $user_to = $row['user_to'];
+        $user_from = $row['user_from'];
+
+        if($user_to != $userLoggedIn)
+            return $user_to;
+        else
+            return $user_from;
+    }
+
+}
+?>
