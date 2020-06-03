2d88e5c7d6668eeafbf3c66811d6972554dd2438
Laura LOspitao <lauralospitao@gmail.com>
2020-06-03 11:39:47 +0200

    #93 Profile Page Messages - Adding Our Message System

--- profile.php
+++ profile.php
@@ -71,8 +71,53 @@ if(isset($_POST['respond_request'])) {
 </div>
 
 <div class="profile_main_column column">
-    <div class="posts_area"></div>
-    <img id="loading" src="assets/images/icons/loading.gif">
+    <ul class="nav nav-tabs" role="tablist" id="profiletabs">
+
+        <li class="active">
+            <a class="nav-link active" href="#newsfeed_div" aria-controls="newsfeed_div" role="tab" data-toggle="tab">Newsfeed</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#messages_div" aria-controls="messages_div" role="tab" data-toggle="tab">Messages</a>
+        </li>
+    </ul>
+
+    <div class="tab-content">
+
+        <div role="tabpanel" class="tab-pane fade in active" id="newsfeed_div">
+            <div class="posts_area"></div>
+            <img id="loading" src="assets/images/icons/loading.gif">
+        </div>
+
+
+        <div role="tabpanel" class="tab-pane fade" id="messages_div">
+                    <?php
+                    $message_obj = new Message($conn, $userLoggedIn);
+                        echo "<h4>You and <a href='" . $username . "'>" . $profile_user_obj->getFirstAndLastName() . "</a></h4><hr><br>";
+                        echo "<div class= 'loaded_messages' id='scroll_messages'>";
+                        echo $message_obj->getMessages($username);
+                        echo"</div>";
+
+                    ?>
+                    <div class="message_post">
+                        <form action="" method="POST">
+                            <textarea name='message_body' id='message_textarea' placeholder='Write your message...'></textarea>
+                            <input type='submit' name='post_message' class='info' id='message_submit' value='Send'>
+                        </form>
+                    </div>
+                    <script>
+                        var div = document.getElementById("scroll_messages");
+
+                        if(div != null) {
+                            div.scrollTop = div.scrollHeight;
+                        }
+                    </script>
+        </div>
+
+    </div>
+
+
+
 
 
 
