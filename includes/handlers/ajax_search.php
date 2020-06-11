ddd0f6c5f2d60d3ec073d77b077ee6474dbf44d3
Laura LOspitao <lauralospitao@gmail.com>
2020-06-11 17:12:12 +0200

    #129 Debbugging

--- includes/handlers/ajax_search.php
+++ includes/handlers/ajax_search.php
@@ -7,21 +7,22 @@ $userLoggedIn = $_POST['userLoggedIn'];
 
 $names = explode(" ", $query);
 
-//if query contains an underscore, assume user is searching for usernames
+//If query contains an underscore, assume user is searching for usernames
 if(strpos($query, '_') !== false)
     $usersReturnedQuery = mysqli_query($conn, "SELECT * FROM users WHERE username LIKE '$query%' AND user_closed='no' LIMIT 8");
-//if there are two words, assume they are first and last name respectively
+//If there are two words, assume they are first and last names respectively
 else if(count($names) == 2)
-    $usersReturnedQuery = mysqli_query($conn, "SELECT * FROM users WHERE (first_name LIKE '$names[0]½' AND last_name LIKE '$names[1]½')  AND user_closed='no' LIMIT 8");
-//if query has one word only, search first names or last names
+    $usersReturnedQuery = mysqli_query($conn, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' AND last_name LIKE '$names[1]%') AND user_closed='no' LIMIT 8");
+//If query has one word only, search first names or last names
 else
-    $usersReturnedQuery = mysqli_query($conn, "SELECT * FROM users WHERE (first_name LIKE '$names[0]½' AND last_name LIKE '$names[0]½')  AND user_closed='no' LIMIT 8");
+    $usersReturnedQuery = mysqli_query($conn, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' OR last_name LIKE '$names[0]%') AND user_closed='no' LIMIT 8");
+
 if($query != "") {
     while($row = mysqli_fetch_array($usersReturnedQuery)) {
         $user = new User($conn, $userLoggedIn);
 
         if($row['username'] != $userLoggedIn)
-            $mutual_friends = user->getMutualFriends($row['username']) . "friends in common";
+            $mutual_friends = $user->getMutualFriends($row['username']) . " friends in common";
         else
             $mutual_friends == "";
 
@@ -38,6 +39,7 @@ if($query != "") {
                 </div>
             </a>
             </div>";
+
     }
 }
 ?>
