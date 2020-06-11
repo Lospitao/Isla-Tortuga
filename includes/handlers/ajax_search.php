3682612e1152fe46d51befaabbb41f9971b93b32
Laura LOspitao <lauralospitao@gmail.com>
2020-06-11 16:44:17 +0200

    #127 Deciding which Query to use

--- includes/handlers/ajax_search.php
+++ includes/handlers/ajax_search.php
@@ -7,4 +7,13 @@ $userLoggedIn = $_POST['userLoggedIn'];
 
 $names = explode(" ", $query);
 
+//if query contains an underscore, assume user is searching for usernames
+if(strpos($query, '_') !== false)
+    $usersReturnedQuery = mysqli_query($conn, "SELECT * FROM users WHERE username LIKE '$query%' AND user_closed='no' LIMIT 8");
+//if there are two words, assume they are first and last name respectively
+else if(count($names) == 2)
+    $usersReturnedQuery = mysqli_query($conn, "SELECT * FROM users WHERE (first_name LIKE '$names[0]½' AND last_name LIKE '$names[1]½')  AND user_closed='no' LIMIT 8");
+//if query has one word only, search first names or last names
+else
+    $usersReturnedQuery = mysqli_query($conn, "SELECT * FROM users WHERE (first_name LIKE '$names[0]½' AND last_name LIKE '$names[0]½')  AND user_closed='no' LIMIT 8");
 ?>
